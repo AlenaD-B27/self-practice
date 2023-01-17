f83537b441100184c2ae4fc44399785b9cc88396
AlenaD-B27 <lenador.18@gmail.com>
2023-01-17 17:56:20 -0600

    Refactored Driver.getDriver()

--- src/test/java/com/project_name/pages/BasePage.java
+++ src/test/java/com/project_name/pages/BasePage.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.support.PageFactory;
 
 public abstract class BasePage {
 	public BasePage() {
-		PageFactory.initElements(Driver.get(), this);
+		PageFactory.initElements(Driver.getDriver(), this);
 	}
 
 
